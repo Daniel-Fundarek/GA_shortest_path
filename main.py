855a44c31d5ba974b84a3d70202cf5972a62aedf
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2022-09-30 16:37:22 +0200

    refactored from 3D list to 2D list

--- main.py
+++ main.py
@@ -16,19 +16,21 @@ def draw_point(_canvas, _point, size):
     _canvas.create_oval(x1, y1, x2, y2)
 
 
-def draw_points(_canvas, _area, _row, _c):
-    for curr_row in range(_row):
-        for curr_column in range(_c):
-            position = _area[curr_row][curr_column]
-            # print(position)
-            draw_point(_canvas, position, 2)
+def draw_points(_canvas, _area):
+    for index in range(len(_area)):
+
+        position = _area[index]
+        # print(position)
+        draw_point(_canvas, position, 2)
 
 
 def create_map(_row, _column):
-    area = numpy.zeros((_row, _column), list)
+    area = []
+    counter = 0
     for curr_row in range(_row):
         for curr_column in range(_column):
-            area[curr_row][curr_column] = [curr_row * step, curr_column * step]
+            area.append([curr_row * step, curr_column * step])
+            counter += 1
     #  print(area)
     return area
 
@@ -47,13 +49,15 @@ step = 10
 ySelection = [i for i in range(0, r * step, step)]
 xSelection = [i for i in range(0, c * step, step)]
 obstacles = []
-
+# obstacle generation
 for i in range(10):
     y = random.choice(ySelection)
     x = random.choice(xSelection)
     y1 = random.choice(ySelection)
     x1 = random.choice(xSelection)
     obstacles.append([[x, y], [x1, y1]])
+###########
+
 
 # print(ySelection, xSelection)
 top = tkinter.Tk()
@@ -61,7 +65,7 @@ area = create_map(r, c)  # x -> <0,190> step 10 y -> <0,990
 offset = 5
 
 canvas = tkinter.Canvas(top, bg="white", height=r * step + 50, width=c * step + 50)
-draw_points(canvas, area, r, c)
+draw_points(canvas, area)
 canvas.pack()
 top.mainloop()
-print('obstacles ',obstacles)
+print('obstacles ', obstacles)
