5680f5dd86ac354536d2376a0dbe5a66b1634204
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2022-10-02 20:16:06 +0200

    working

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,6 +1,14 @@
 import random
 from math import sqrt
 import copy
+from shapely.geometry import LineString
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 def generate_chromosome(num_genes):
     chromosome = []
@@ -8,7 +16,7 @@ def generate_chromosome(num_genes):
         chromosome.append(random.randint(0, 19))
     chromosome[0] = 10
     chromosome[99] = 10
-    #print(chromosome)
+    # print(chromosome)
     return chromosome
 
 
@@ -25,7 +33,7 @@ def mutate(population, rate, genes_amount):
     population_indexes = random.sample(range(0, len(_population)), round(len(_population) * rate))
 
     for population_index in population_indexes:
-        chromosome_indexes = random.sample(range(1, len(_population[0]) - 1), genes_amount)
+        chromosome_indexes = random.sample(range(1, len(_population[population_index]) - 1), genes_amount)
         for chromosome_index in chromosome_indexes:
             _population[population_index][chromosome_index] = random.randint(0, 19)
     return _population
@@ -60,7 +68,16 @@ def cross(population, rate, genes_amount):
     return _population
 
 
-def calculate_fitness(population, area):
+def invert(population, rate):
+    _population = copy.deepcopy(population)
+    population_indexes = []
+    population_indexes = random.sample(range(0, len(_population)), round(len(_population) * rate))
+    for population_index in population_indexes:
+        _population[population_index] = _population[population_index][::-1]
+    return _population
+
+
+def calculate_fitness(population, area, obstacles):
     fit = []
     for _chromosome in population:
         length = 0
@@ -72,18 +89,65 @@ def calculate_fitness(population, area):
             y0 = point0[0]
             x1 = point1[1]
             y1 = point1[0]
-            c = sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
-            length += c
+            c1 = sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            a = Point(x0, y0)
+            b = Point(x1, y1)
+            if index % 2 == 0:
+                c = Point(obstacles[int(index / 2)][0][0], obstacles[int(index / 2)][0][1])
+                d = Point(obstacles[int(index / 2)][1][0], obstacles[int(index / 2)][1][1])
+                x, y = get_intersect(a, b, c, d)
+                if x != -50:
+                    multiplier = min(abs(y - obstacles[int(index / 2)][0][1]), abs(y - obstacles[int(index / 2)][1][1]))
+                    length += 4000 * (multiplier +1)
+                # print('true')
+                else:
+                    pass
+                # print('false')
+
+            # line = LineString([(x0, y0), (x1, y1)])
+            # second_line = LineString(obstacles[index])
+            # if line.intersects(second_line):
+            #     print('true')
+            #     length += 300
+            # else:
+            #     print('false')
+            # for obstacle in obstacles:
+            #    second_line = LineString(obstacle)
+            #    if(line.intersects(second_line)):
+            #       length += 300
+            length += c1
         fit.append(length)
     return fit
 
 
+def get_intersect(A, B, C, D):
+    # a1x + b1y = c1
+    a1 = B.y - A.y
+    b1 = A.x - B.x
+    c1 = a1 * (A.x) + b1 * (A.y)
+
+    # a2x + b2y = c2
+    a2 = D.y - C.y
+    b2 = C.x - D.x
+    c2 = a2 * (C.x) + b2 * (C.y)
+
+    # determinant
+    det = a1 * b2 - a2 * b1
+
+    # parallel line
+    if det != 0:
+        x = ((b2 * c1) - (b1 * c2)) / det
+        y = ((a1 * c2) - (a2 * c1)) / det
+        return (x, y)
+    return -100, -100
+
+
 def sort(population, fitness):
-    sortedList = sorted(zip(fitness,population))
-    p =[]
+    sortedList = sorted(zip(fitness, population))
+    p = []
     f = []
-    p , f = zip(*sortedList)
-    return list(p),list(f)
+    p, f = zip(*sortedList)
+    return list(p), list(f)
 
 
 # sel best always sort- to save computation sort always on the top just once
@@ -91,7 +155,7 @@ def select_best(population, fitness, num):
     new_pop = []
     new_pop = population[:num]
     new_pop = copy.deepcopy(new_pop)
-    return new_pop#, fitness[::num]
+    return new_pop  # , fitness[::num]
 
 
 def select_random(population, fitness, num):
@@ -99,11 +163,10 @@ def select_random(population, fitness, num):
     p = []
     f = []
     for i in population_indexes:
-
         p.append(population[i])
         f.append(fitness[i])
         new_pop = copy.deepcopy(p)
-    return new_pop#, f
+    return new_pop  # , f
 
 
 def select_tourn(population, fitness, num):
--- main.py
+++ main.py
@@ -29,12 +29,12 @@ def draw_points(_canvas, _area):
         draw_point(_canvas, position, 2)
 
 
-def create_obstacles(c, step):
+def create_obstacles(c, step, num):
     # obstacle generation
     _obstacles = []
-    for i in range(0, c * step, step * 10):
+    for i in range(0, c * step, step * 2):
         x = i
-        x1 = i + random.randrange(10, 80, 10)
+        x1 = i + 10  # random.randrange(10, 80, 10)
         y = random.choice(ySelection)
         y1 = random.choice(ySelection)
         _obstacles.append([[x, y], [x1, y1]])
@@ -70,9 +70,9 @@ def draw_lines(canvas, _chromosome, area):
         # print((_chromosome[index]) * 100 + index,_chromosome[index - 1],index)
         point1 = area[(_chromosome[index]) * 100 + index]
         # print(point0, point1)
-        x0 = point0[1]   + offset
+        x0 = point0[1] + offset
         y0 = point0[0] + offset
-        x1 = point1[1]   + offset
+        x1 = point1[1] + offset
         y1 = point1[0] + offset
         canvas.create_line(x0, y0, x1, y1, fill='green')
 
@@ -84,13 +84,13 @@ def draw_lines(canvas, _chromosome, area):
 r = 20
 c = 100
 step = 10
-generation = 3000
+generation = 5000
 ga = GeneticAlgorithm
 ySelection = [i for i in range(10, r * step, step)]
-obstacles = create_obstacles(c, step)
+obstacles = create_obstacles(c, step, 100)
 # chromosome = ga.generate_chromosome(100)
 population = []
-population = ga.generate_population(100, 50)
+population = ga.generate_population(100, 100)
 
 # print('population:', population)
 # population = ga.mutate(population,0.6,50)
@@ -102,13 +102,17 @@ area = create_map(r, c)  # x -> <0,190> step 10 y -> <0,990
 offset = 5
 fitness = []
 evolution = []
+multiplier = 1 / generation
 for i in range(generation):
-    fitness = ga.calculate_fitness(population, area)
+    fitness = ga.calculate_fitness(population, area, obstacles)
     newPop = []
     work1 = []
     work2 = []
     work3 = []
-
+    work4 = []
+    work5 = []
+    work6 = []
+    work7 = []
     [fitness, population] = ga.sort(population, fitness)
     evolution.append(min(fitness))
     newPop = ga.select_best(population, fitness, 3)
@@ -121,12 +125,29 @@ for i in range(generation):
     work2 = ga.select_random(population, fitness, 15)
     work3 = ga.select_best(population, fitness, 2)
 
-    work1 = ga.additive_mutate(work1, 0.3, 50, [-3, 3])
-    work2 = ga.cross(work2, 0.3, 30)
-    work3 = ga.mutate(work3, 0.3, 1)
+    work1 = ga.additive_mutate(work1, i * multiplier, 50, [-3, 3])
+    work2 = ga.cross(work2, 0.5, 50)
+    work3 = ga.mutate(work3, 0.6, 3)
+
+    work4 = ga.select_random(population, fitness, 14)
+    work4.extend(ga.select_best(population, fitness, 1))
+
+    work5 = ga.select_random(population, fitness, 13)
+    work5.extend(ga.select_best(population, fitness, 1))
+
+    work6 = ga.select_best(population, fitness, 1)
+
+    work4 = ga.additive_mutate(work4, 0.2, 2, [-1, 1])
+    work5 = ga.invert(work5, 0.3)
+    work6 = ga.mutate(work6, i * multiplier, random.randint(1, 99))
+    work7 = ga.generate_population(100, 20)
     newPop.extend(work1)
     newPop.extend(work2)
     newPop.extend(work3)
+    newPop.extend(work4)
+    newPop.extend(work5)
+    newPop.extend(work6)
+    newPop.extend(work7)
     population = newPop
     print(f'Evolution number:{i} fitness: {evolution[i]} ')
 # drawing
@@ -134,13 +155,13 @@ print(evolution)
 '''
 x = [i for i in range(500)]
 fig, ax = plt.subplots()
-ax.plot(x,evolution,color='green')
+ax.plot(x,evolution)
 fig.savefig("evolution.pdf")
 fig.show()
 
 # print('obstacles ', obstacles)
 
-
+''''''
 # Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
